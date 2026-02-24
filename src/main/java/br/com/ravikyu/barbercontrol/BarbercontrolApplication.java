192810e07c47f9ee3a9121c19fd864d33966e09f
Yuri Ravik <69877264+yuriravik@users.noreply.github.com>
2026-02-23 22:24:17 -0300

    comit inicial java

---
+++ src/main/java/br/com/ravikyu/barbercontrol/BarbercontrolApplication.java
@@ -0,0 +1,13 @@
+package br.com.ravikyu.barbercontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbercontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbercontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ravikyu/barbercontrol/BarbercontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ravikyu.barbercontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbercontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
